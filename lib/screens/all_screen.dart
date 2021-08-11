e04f91043f3677e68b6803de03f80d493a75433f
hashiim <hashmhssn@gmail.com>
2021-08-12 00:20:55 +0300

    filter

--- lib/screens/all_screen.dart
+++ lib/screens/all_screen.dart
@@ -9,35 +9,83 @@ class AllScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final ListsProvider prov =
         Provider.of<ListsProvider>(context, listen: true);
-    return ListView.builder(
-      itemCount: prov.allIteams.length,
-      itemBuilder: (context, index) => Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            onLongPress: () {
-              ScaffoldMessenger.of(context).showSnackBar(mySnackBar(
-                  " ${prov.allIteams[index].name} has been deleted"));
-              prov.deleteFromAll(index);
-            },
-            onTap: () {
-              prov.addToNeed(index);
-            },
-            child: Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                border: Border.all(color: Color(0xffbc6c25)),
-                color: Color(0xfffefae0),
-              ),
-              child: Text(
-                prov.allIteams[index].name,
-                style: TextStyle(fontSize: 24),
+    return Stack(children: [
+      ListView.builder(
+        itemCount: prov.allIteams.length,
+        itemBuilder: (context, index) => Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onLongPress: () {
+                ScaffoldMessenger.of(context).showSnackBar(mySnackBar(
+                    " ${prov.allIteams[index].name} has been deleted"));
+                prov.deleteFromAll(index);
+              },
+              onTap: () {
+                prov.addToNeed(index);
+              },
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  border: Border.all(color: Color(0xffbc6c25)),
+                  color: Color(0xfffefae0),
+                ),
+                child: Text(
+                  prov.allIteams[index].name,
+                  style: TextStyle(fontSize: 24),
+                ),
               ),
             ),
           ),
         ),
       ),
-    );
+      Positioned(
+        bottom: 50,
+        right: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: createMaterialColor(Color(0xff606c38)),
+          ),
+          child: IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Name'),
+                    actions: <Widget>[
+                      TextField(),
+                      ToggleButtons(
+                        children: [
+                          Icon(Icons.ac_unit),
+                          Icon(Icons.accessible_sharp)
+                        ],
+                        isSelected: [false, true],
+                      )
+                    ],
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.add),
+          ),
+        ),
+      ),
+      Positioned(
+        bottom: 50,
+        left: 50,
+        child: Container(
+          decoration: BoxDecoration(
+              color: createMaterialColor(Color(0xffbc6c25)),
+              shape: BoxShape.circle),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.filter_list),
+          ),
+        ),
+      ),
+    ]);
   }
 }
